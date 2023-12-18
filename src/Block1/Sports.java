dbd30e7cfc16c96862958881e5de546ef7701445
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-12-18 21:40:05 +0300

    Домашка ООП-2

---
+++ src/Block1/Sports.java
@@ -0,0 +1,11 @@
+package Block1;
+
+public class Sports extends Bicycle{
+    private int speeds;
+
+    public Sports(String modelName, int wheelsCount, int speeds) {
+        super(modelName, wheelsCount);
+        this.speeds = speeds;
+    }
+
+}
--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    //Домашка ООП2
-    public static void main(String[] args) {
-
-
-    }
-}
